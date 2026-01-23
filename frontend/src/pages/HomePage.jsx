25ee5a680b896aae2fcfb358cba0ba4f128fe6d1
varshneylove <lovevarshney7002@gmail.com>
2026-01-23 20:57:47 +0530

    homePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import {FaArrowRight} from "react-icons/fa"
 import {Link} from "react-router-dom"
+import HighlightText from '../component/core/HomePage/HighlightText'
+import CTAButton from '../component/core/HomePage/Button'
+import Banner from "../Asset/Image/banner.mp4"
+import CodeBlocks from "../component/core/HomePage/CodeBlock"
 
 const Home = () => {
 
     return (
-        <div className='relative mx-auto flex flex-col w-11/12 items-center text-white justify-between '>
+        <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between '>
 
-            {/* Section 1 */}
-
+           
             <div>
                 <Link to={"/signup"}>
-                   <div>
-                    <div>
+                   <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit'>
+                    <div className='flex flex-row items-center gap-2  rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900'>
                         <p>
                             Become an Instructor
                         </p>
@@ -20,6 +23,66 @@ const Home = () => {
                     </div>
                    </div>
                 </Link>
+
+                <div className='text-center text-4xl font-semibold mt-7'>
+                    Empower Your Future with
+                    <HighlightText text={"Coding Skills"}/>
+                </div>
+
+                <div className='mt-4 text-center w-[90%] text-lg font-bold text-richblack-300  '>
+                    With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors. 
+                </div>
+
+                <div className='flex flex-row gap-7 mt-8 justify-center'>
+                    <CTAButton active={true} linkto={"/signup"}>
+                        Learn More
+                    </CTAButton>
+                    <CTAButton active={false} linkto={"/login"}>
+                        Book Demo
+                    </CTAButton>
+                </div>
+
+
+                <div className= 'shadow-blue-200 mx-3 my-15'>
+                    <video
+                    muted 
+                    loop
+                    autoPlay
+                    > <source src={Banner} type="video/mp4" />
+                    </video>
+                </div>
+
+               {/* Section 1 */}
+
+                <div>
+                        <CodeBlocks 
+                        position={`lg:flex-row`}
+                        heading={
+                            <div className='text-4xl font-semibold'>
+                                Unlpck Your
+                                <HighlightText text={"coding courses"} /> 
+                                with our online courses
+                                </div> 
+                        }
+                        subheading={
+                            "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+                        }
+                        ctabtn1={
+                            {
+                                btnText:"Try it yourself",
+                                linkto:"/signup",
+                                active:true,
+                            }
+                        }
+                        ctabtn2={
+                            {
+                                btnText:"Learn More",
+                                linkto:"/login",
+                                active:false,
+                            }
+                        }
+                        />
+                </div>
 
             </div>
    
