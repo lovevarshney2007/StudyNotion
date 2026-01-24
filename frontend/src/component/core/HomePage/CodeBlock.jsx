d81d26b45348344988e18b5230d749f3ed14cafb
varshneylove <lovevarshney7002@gmail.com>
2026-01-24 12:45:24 +0530

    session 1 completed

--- frontend/src/component/core/HomePage/CodeBlock.jsx
+++ frontend/src/component/core/HomePage/CodeBlock.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import CTAButton from "./Button"
 import HighlightText from './HighlightText'
 import {FaArrowRight} from "react-icons/fa"
+import {TypeAnimation} from 'react-type-animation'
 
 const CodeBlock = ({
     position,heading,subheading,ctabtn1,ctabtn2,codeblock,backgroundGrdient,codeColor,
 }) => {
   return (
-    <div className={`flex ${position} my-20 justify-between gap-10`}>
+    <div className={`flex ${position} my-20 justify-between flex-col lg:gap-10 gap-10`}>
 
         {/*Section 1 */}
-        <div className='w-[50%] flex flex-col gap-8'>
+        <div className='w-[100%] lg:w-[50%] flex flex-col gap-8'>
             {heading}
-            <div className='text-richblack-300 font-bold'>
+
+            {/* SubHeading  */}
+            <div className='text-richblack-300 text-base font-bold w-[85%] -mt-3'>
                 {subheading}
             </div>
 
@@ -23,15 +26,55 @@ const CodeBlock = ({
                         <FaArrowRight />
                     </div>
                 </CTAButton>
-            </div>
 
-            <div className='flex gap-7 mt-7'>
-                <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>
+                  <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>
                    {ctabtn2.btnText}
                 </CTAButton>
+
             </div>
+
         </div>
         
+
+        {/* {Session 2} */}
+        <div className='relative flex flex-row h-fit text-[10px] w-[100%] py-4 lg:w-[500px] border-2 border-richblack-800 rounded-lg'>
+            {/* Hw: Bg-> Gradient  */}
+
+            <div className={`absolute ${backgroundGrdient} w-[370px] h-[257px] rounded-full blur-[80px] opacity-20 -left-[10%] -top-[10%] -z-10 animate-pulse`}></div>
+
+            <div className='text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold'>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+                <p>6</p>
+                <p>7</p>
+                <p>8</p>
+                <p>9</p>
+                <p>10</p>
+                <p>11</p>
+            </div>
+
+           {/* Code Animation  */}
+        <div className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColor} pr-2`}>
+             <TypeAnimation
+             sequence={[codeblock,2000,""]}
+             repeat={Infinity}
+             cursor={true}
+             style={
+                {
+                    whiteSpace:"pre-line",
+                    display:"block",
+                    fontSize:"14px"
+                }
+             }
+             omitDeletionAnimation={true}
+
+             />
+            </div>
+        </div>
+
     </div>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between '>
 
-           
+             {/*  Section 1 */}
             <div>
                 <Link to={"/signup"}>
                    <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit'>
@@ -52,14 +52,14 @@ const Home = () => {
                     </video>
                 </div>
 
-               {/* Section 1 */}
+             {/* Code Section 1 */}
 
                 <div>
                         <CodeBlocks 
                         position={`lg:flex-row`}
                         heading={
                             <div className='text-4xl font-semibold'>
-                                Unlpck Your
+                                Unblock Your
                                 <HighlightText text={"coding courses"} /> 
                                 with our online courses
                                 </div> 
@@ -81,8 +81,56 @@ const Home = () => {
                                 active:false,
                             }
                         }
+
+
+                            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+                            codeColor={"text-yellow-25"}
+                            backgroundGrdient={"bg-gradient-to-br from-yellow-400 to-orange-500"}
+
                         />
                 </div>
+
+                
+             {/* Code Section 2 */}
+
+               <div>
+                        <CodeBlocks 
+                        position={`lg:flex-row-reverse`}
+                        heading={
+                            <div className='text-4xl font-semibold'>
+                                Start
+                                <HighlightText text={"coding courses"} /> 
+                                coding in seconds
+                                </div> 
+                        }
+                        subheading={
+                            "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+                        }
+                        ctabtn1={
+                            {
+                                btnText:"Continue Lesseon",
+                                linkto:"/signup",
+                                active:true,
+                            }
+                        }
+                        ctabtn2={
+                            {
+                                btnText:"Learn More",
+                                linkto:"/login",
+                                active:false,
+                            }
+                        }
+
+
+                            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+                            codeColor={"text-white"}
+                            backgroundGrdient={"bg-gradient-to-br from-blue-400 to-cyan-500"}
+
+
+                        />
+                </div>
+
+                
 
             </div>
    
@@ -101,3 +149,6 @@ const Home = () => {
 
 
 export default Home
+
+
+
